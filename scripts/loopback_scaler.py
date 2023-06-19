fce330c9cc1b15d8d9ca85401bab0f028c706d31
Alexander Brown <DrJKL0424@gmail.com>
2023-06-18 22:53:57 -0700

    Show one set of size controls at a time

--- scripts/loopback_scaler.py
+++ scripts/loopback_scaler.py
@@ -13,6 +13,12 @@ from PIL import ImageFilter, ImageEnhance
 # This modification came from a process that I learned from the AI community to improve details and prepare an
 # image for post-processing.
 
+def show_scale(use_scale):
+    return gr.Slider.update(visible=use_scale)
+
+def show_wh(use_scale):
+    return [gr.Slider.update(visible=(not use_scale))]*2
+
 class Script(scripts.Script):
     def title(self):
         return "Loopback Scaler"
@@ -21,6 +27,8 @@ class Script(scripts.Script):
         return is_img2img
     help_text = "<strong>Loops:</strong> The number of times the script will inference your image and increase the resolution in increments. The amount the resolution is increased each loop is determined by this number and the maximum image width/height.  The more loops, the more chances of your image picking up more detail, but also artifacts.  4 to 10 is what I find to work best, but you may like more or less.<br><br><strong>Denoise change:</strong> This setting will increase or decrease the denoising strength every loop.  A higher value will increase the denoising strength, while a lower value will decrease it. A setting of 1 keeps the denoising strength as it is set on the img2img settings.<br><br><strong>Adaptive change:</strong> This setting changes the amount of resolution increase per loop, keeping the changes from being linear.  The higher the value the more significant the resolution changes toward the end of the looping.<br><br><strong>Maximum Image Width/Height:</strong> These parameters set the maximum width and height of the final image. Always start with an image smaller than these dimensions.  The smaller you start, the more impressive the results. I usually start at either 340x512 or 512x768<br><br><strong>Detail, Blur, Smooth, Contour:</strong> These parameters are checkboxes that apply a PIL Image Filter to the final image.<br><br><strong>Sharpness, Brightness, Color, Contrast:</strong> These parameters are sliders that adjust the sharpness, brightness, color, and contrast of the image. 1 will result in no adjustments, less than one reduces these settings for the final image and greater than 1 increases these settings.<br><br><strong>Img2Img Settings:</strong>  I recommend creating an image with txt2img and then sending the result to img2img with the prompt and settings.  For this script I use these settings..<br><br><strong>Resize mode -</strong> Crop and resize<br><strong>Sampling method -</strong> DDIM<br><strong>Sampling steps -</strong> 30<br><strong>Width/Height -</strong> 340x512 or 512x768.  I’d try to keep to the aspect ratio of the original image but these can be set lower than the resolution of the original image<br><strong>CFG Scale -</strong> 6 to 8<br><strong>Denoising strength -</strong> 0.2 to 0.4 is usual.  The lower you go, the less change between loops.  The higher you go the less the end result will look like the original image.<br><strong>Seed -</strong> This doesn’t matter too much, I usually keep it at -1</p>"
     detail_choices = ["None", "Low", "Medium", "High"]
+    
+    
     def ui(self, is_img2img):
         with gr.Blocks():
             with gr.Box():
@@ -29,27 +37,29 @@ class Script(scripts.Script):
                     denoising_strength_change_factor = gr.Slider(minimum=0.9, maximum=1.1, step=0.01, label='Denoise Change:', value=1, elem_id=self.elem_id("denoising_strength_change_factor"))            
                     adaptive_increment_factor = gr.Slider(minimum=0.5, maximum=2.0, step=0.1, label='Adaptive Change:', value=1.0, elem_id=self.elem_id("adaptive_increment_factor"))
             with gr.Box():
+                use_scale = gr.Checkbox(label='Use Scale', value=False, elem_id=self.elem_id("use_scale"))
                 with gr.Row():
                     max_width = gr.Slider(minimum=512, maximum=4096, step=64, label='Maximum Image Width:', value=1024, elem_id=self.elem_id("max_width"))
                     max_height = gr.Slider(minimum=512, maximum=4096, step=64, label='Maximum Image Height:', value=1024, elem_id=self.elem_id("max_height"))
                 with gr.Row():
-                    use_scale =  gr.Checkbox(label='Use Scale', value=False, elem_id=self.elem_id("use_scale"))
-                    scale = gr.Slider(minimum=.5, maximum=4, step=.1, label='Scale Final Image:', value=1, elem_id=self.elem_id("final_image_scale"))
-            with gr.Box():
+                    scale = gr.Slider(minimum=.5, maximum=4, step=.1, label='Scale Final Image:', value=1, elem_id=self.elem_id("final_image_scale"), visible=False)
+            with gr.Accordion("Final Image Modification", open=False):
                 with gr.Row():
                     detail_strength = gr.Dropdown(label='Add Detail', choices=self.detail_choices, value="None", elem_id=self.elem_id("detail_strength"))
                     blur_strength = gr.Dropdown(label='Add Blur', choices=self.detail_choices, value="None", elem_id=self.elem_id("blur_bool"))
                     smooth_strength = gr.Dropdown(label='Smoothing', choices=self.detail_choices, value="None", elem_id=self.elem_id("smooth_strength"))
                     contour_bool = gr.Checkbox(label='Contour', value=False, elem_id=self.elem_id("contour_bool"))
-            with gr.Box():
-                with gr.Row():
-                    sharpness_strength = gr.Slider(minimum=0.1, maximum=2.0, step=0.01, label='Sharpness:', value=1.0, elem_id=self.elem_id("sharpness_strength")) 
-                    brightness_strength = gr.Slider(minimum=0.1, maximum=2.0, step=0.01, label='Brightness:', value=1.0, elem_id=self.elem_id("brightness_strength"))
-                with gr.Row():
-                    color_strength = gr.Slider(minimum=0.1, maximum=2.0, step=0.01, label='Color:', value=1.0, elem_id=self.elem_id("color_strength"))
-                    contrast_strength = gr.Slider(minimum=0.1, maximum=2.0, step=0.01, label='Contrast:', value=1.0, elem_id=self.elem_id("contrast_strength"))
+                with gr.Box():
+                    with gr.Row():
+                        sharpness_strength = gr.Slider(minimum=0.1, maximum=2.0, step=0.01, label='Sharpness:', value=1.0, elem_id=self.elem_id("sharpness_strength")) 
+                        brightness_strength = gr.Slider(minimum=0.1, maximum=2.0, step=0.01, label='Brightness:', value=1.0, elem_id=self.elem_id("brightness_strength"))
+                    with gr.Row():
+                        color_strength = gr.Slider(minimum=0.1, maximum=2.0, step=0.01, label='Color:', value=1.0, elem_id=self.elem_id("color_strength"))
+                        contrast_strength = gr.Slider(minimum=0.1, maximum=2.0, step=0.01, label='Contrast:', value=1.0, elem_id=self.elem_id("contrast_strength"))
             with gr.Accordion("Info - Loopback Scaler", open=False):
                 helpinfo = gr.HTML("<p style=\"margin-bottom:0.75em\">{}</p>".format(self.help_text))
+        use_scale.change(show_scale, inputs=use_scale, outputs=scale)
+        use_scale.change(show_wh, inputs=use_scale, outputs=[max_width, max_height])
         return [helpinfo, loops, denoising_strength_change_factor, max_width, max_height, scale, use_scale, detail_strength, blur_strength, contour_bool, smooth_strength, sharpness_strength, brightness_strength, color_strength, contrast_strength, adaptive_increment_factor]
 
     def __get_width_from_ratio(self, height, ratio):
@@ -105,7 +115,7 @@ class Script(scripts.Script):
         use_height = current_ratio >= max_ratio
                     
         #set loop increment to the lower of height/width and height if equal
-        if current_ratio < max_ratio:
+        if not use_height:
             #width will hit max first
             loop_increment = math.floor((final_width - p.width)/loops)
         else:
